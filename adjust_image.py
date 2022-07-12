211cb2d80f03f7b5013bc0229df39b10fc96fe19
yzgd <31798291+yzgd@users.noreply.github.com>
2022-07-12 13:22:22 +0800

    Bug fix

--- adjust_image.py
+++ adjust_image.py
@@ -164,7 +164,7 @@ read_params("adjust_image_params.xlsx")
 if len(sys.argv)>1:
     working_dir = sys.argv[1]
 if len(sys.argv)>2:
-    gamma = sys.argv[2]
+    gamma = int(sys.argv[2])
 
 print("Working directory:         %s" %(working_dir))
 print("Gamma:                     %s" %(gamma))
